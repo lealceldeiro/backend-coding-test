bb6095062c7ee5e1dbea08fee630099abe90dfdd
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2022-01-15 18:09:25 +0200

    Add SubtaskEntity
    - Update TaskEntity model to hold subtasks

---
+++ src/main/java/com/example/demo/subtaskentity/SubtaskEntity.java
@@ -0,0 +1,57 @@
+package com.example.demo.subtaskentity;
+
+import com.example.demo.task.TaskEntity;
+import org.hibernate.Hibernate;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Objects;
+
+// It's not clear whether a subtask is actually a task (maybe with some more attributes), so I created a brand-new class
+// as stated in the README.md file. If a subtask is going to hold the same attributes that a task holds, then a
+// composite structure could have been created using the same TaskEntity
+@Entity
+public class SubtaskEntity {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private TaskEntity parentTask;
+
+    // region getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+    // endregion
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) {
+            return false;
+        }
+        SubtaskEntity entity = (SubtaskEntity) other;
+        return Objects.equals(id, entity.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "SubtaskEntity{id=" + id + '}';
+    }
+}
--- src/main/java/com/example/demo/task/TaskEntity.java
+++ src/main/java/com/example/demo/task/TaskEntity.java
@@ -1,9 +1,18 @@
 package com.example.demo.task;
 
+import com.example.demo.subtaskentity.SubtaskEntity;
+import org.hibernate.Hibernate;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class TaskEntity {
@@ -18,7 +27,11 @@ public class TaskEntity {
 
     private LocalDateTime createdAt;
 
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private Set<SubtaskEntity> subtasks;
+
     public TaskEntity() {
+        this.subtasks = new HashSet<>();
     }
 
     public TaskEntity(String description, boolean completed, TaskPriority priority, LocalDateTime createdAt) {
@@ -26,8 +39,10 @@ public class TaskEntity {
         this.completed = completed;
         this.priority = priority;
         this.createdAt = createdAt;
+        this.subtasks = new HashSet<>();
     }
 
+    // region
     public int getId() {
         return id;
     }
@@ -66,5 +81,36 @@ public class TaskEntity {
 
     public void setCreatedAt(final LocalDateTime createdAt) {
         this.createdAt = createdAt;
+    }
+    // endregion
+
+    public void addSubtask(SubtaskEntity subtaskEntity) {
+        subtasks.add(subtaskEntity);
+    }
+
+    public void deleteSubtask(SubtaskEntity subtaskEntity) {
+        subtasks.remove(subtaskEntity);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) {
+            return false;
+        }
+        TaskEntity entity = (TaskEntity) other;
+        return Objects.equals(id, entity.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "TaskEntity{id=" + id + '}';
     }
 }
