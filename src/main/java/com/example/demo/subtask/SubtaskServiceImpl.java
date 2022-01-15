a9b96a586ec22bbdbefd66b1a498ded357d3957c
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2022-01-15 23:45:10 +0200

    Implement SubtaskService
    - Add SubtaskServiceImpl

--- src/main/java/com/example/demo/subtask/SubtaskServiceImpl.java
+++ src/main/java/com/example/demo/subtask/SubtaskServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.demo.subtask;
 
+import com.example.demo.exception.SubtaskNotFoundException;
+import com.example.demo.task.TaskRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -7,31 +10,45 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class SubtaskServiceImpl implements SubtaskService {
-    @Override
-    @Transactional(readOnly = true)
-    public Page<SubtaskDto> getSubtasks(final int taskId, final Pageable pageable) {
-        return null;
-    }
+    private final SubtaskRepository subtaskRepository;
+    private final TaskRepository taskRepository;
+    private final SubtaskTransformer subtaskTransformer;
 
     @Override
     @Transactional(readOnly = true)
-    public SubtaskDto getSubtask(final int taskId, final int subtaskId) {
-        return null;
+    public Page<SubtaskDto> getSubtasks(int taskId, Pageable pageable) {
+        return subtaskRepository.findAllWithParentTaskId(taskId, pageable);
     }
 
     @Override
-    public SubtaskDto createSubtask(final int taskId, final SubtaskDto subtaskDto) {
-        return null;
+    @Transactional(readOnly = true)
+    public SubtaskDto getSubtask(int taskId, int subtaskId) {
+        return subtaskRepository.findAllWithParentTaskIdAndId(taskId, subtaskId)
+                                .orElseThrow(() -> new SubtaskNotFoundException(taskId, subtaskId));
     }
 
     @Override
-    public void updateSubTask(final int taskId, final int subtaskId, final SubtaskDto subtaskDto) {
+    public SubtaskDto createSubtask(int taskId, SubtaskDto subtaskDto) {
+        var task = taskRepository.getOne(taskId);
+        var subtaskEntity = subtaskTransformer.toEntity(subtaskDto);
+        subtaskEntity.setParentTask(task);
 
+        return subtaskTransformer.toIdDto(subtaskRepository.save(subtaskEntity));
     }
 
     @Override
-    public void deleteSubtask(final int taskId, final int subtaskId) {
+    public void updateSubTask(int taskId, int subtaskId, SubtaskDto subtaskDto) {
+        var subtaskEntity = subtaskRepository.findWithParentTaskIdAndId(taskId, subtaskId)
+                                             .orElseThrow(() -> new SubtaskNotFoundException(taskId, subtaskId));
+        subtaskTransformer.updateEntity(subtaskEntity, subtaskDto);
+    }
 
+    @Override
+    public void deleteSubtask(int taskId, int subtaskId) {
+        if (subtaskRepository.deleteEntityWithParentTaskIdAndId(taskId, subtaskId) < 1) {
+            throw new SubtaskNotFoundException(taskId, subtaskId);
+        }
     }
 }
