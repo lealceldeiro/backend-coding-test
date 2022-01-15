b07a7e8c9b642dadd8da57e460f6c1460a585f85
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2022-01-15 23:42:29 +0200

    Update SubtaskDto with method accessors and constructors

--- src/main/java/com/example/demo/subtask/SubtaskDto.java
+++ src/main/java/com/example/demo/subtask/SubtaskDto.java
@@ -1,15 +1,19 @@
 package com.example.demo.subtask;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
-@EqualsAndHashCode
 @ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SubtaskDto {
     private Integer id;
