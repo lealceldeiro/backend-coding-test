ece70262024b3604994adabc2202886920ea350b
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2022-01-16 09:08:01 +0200

    Add test cases
    - TaskEntity

---
+++ src/test/java/com/example/demo/task/TaskEntityTest.java
@@ -0,0 +1,64 @@
+package com.example.demo.task;
+
+import com.example.demo.TestsUtil;
+import org.hibernate.Hibernate;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mockStatic;
+
+class TaskEntityTest {
+    @Test
+    void equalsReturnsTrueForSameReference() {
+        var entity = new TaskEntity();
+        assertTrue(entity.equals(entity));  // do not use assertEquals: the equality comparison is made by junit itself
+    }
+
+    @Test
+    void equalsReturnsFalseIfOtherIsNull() {
+        assertFalse(new TaskEntity().equals(null));
+    }
+
+    @Test
+    void equalsReturnsFalseIfTheUnderlyingClassesOfTheProxiedPersistenceClassesReportedByHibernateAreNotEqual() {
+        var entity1 = TestsUtil.taskEntityStub();
+        var entity2 = TestsUtil.taskEntityStub();
+        try (var mockedHibernate = mockStatic(Hibernate.class)) {
+            mockedHibernate.when(() -> Hibernate.getClass(any(TaskEntity.class)))
+                           .thenReturn(String.class)
+                           .thenReturn(Boolean.class);
+            assertFalse(entity1.equals(entity2));
+        }
+    }
+
+    @Test
+    void equalsReturnsTrueForEntitiesWithEqualsId() {
+        var entity1 = TestsUtil.taskEntityStub();
+        var entity2 = TestsUtil.taskEntityStub();
+        entity2.setId(entity1.getId());
+
+        assertTrue(entity1.equals(entity2));
+    }
+
+    @Test
+    void equalsReturnsFalseForEntitiesWithDifferentId() {
+        var entity1 = TestsUtil.taskEntityStub();
+        var entity2 = TestsUtil.taskEntityStub();
+        entity2.setId(entity1.getId() + 1);
+
+        assertFalse(entity1.equals(entity2));
+    }
+
+    @Test
+    void hashCodeIsClassHasCode() {
+        assertEquals(TaskEntity.class.hashCode(), new TaskEntity().hashCode());
+    }
+
+    @Test
+    void toStringShowsEntityId() {
+        assertTrue(new TaskEntity().toString().contains("id="));
+    }
+}
